3468edcdc4302e22a379f55129c08743ca6e4c6f
e1730934 <e1730934@cmaisonneuve.qc.ca>
2022-09-27 23:08:20 -0400

    Refactored folders

--- public/test.js
+++
@@ -1,6 +0,0 @@
-
-const form = document.getElementById("contact-form");
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    console.log(`${e.target.name.value} ${e.target.email.value} ${e.target.message.value}`);
-})
